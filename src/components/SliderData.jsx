1e88365fb696d6a08c1ebba873b80e1201a5befb
KarlaB3 <k.bucoy@gmail.com>
2023-05-28 15:00:21 +1000

    feat: image slider and styling

---
+++ src/components/SliderData.jsx
@@ -0,0 +1,17 @@
+export const SliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1606477787346-91b6faa76b64?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1522029822044-65b92af9d0c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'
+    },
+    {   
+        image: 'https://images.unsplash.com/photo-1603959628439-64b39246c77a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1973&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1682018062567-25ff110ca995?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2074&q=80'
+    },
+    {   
+        image: 'https://images.unsplash.com/photo-1616797655422-d2c2ae6e20da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1631&q=80'
+    }
+]
